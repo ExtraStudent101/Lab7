6645eff8b8981e23eef4e7d453e65ffe18566779
theerapat_lum@cmu.ac.th <theerapat_lum@cmu.ac.th>
2022-12-13 16:36:48 +0700

    lab7_4 finished

--- lab7_4.cpp
+++ lab7_4.cpp
@@ -9,7 +9,18 @@ int main(){
 	int id[] = {123,196,225,329,420,451,515,666,690,787};
 	double gpa[] = {1.50,2.12,4.00,3.69,2.50,2.76,3.54,2.14,3.50,1.88};
 
-    //Write your code here.
+
+   double x;
+   int n = 0;
+    cout << "Please input GPA: " ;
+	cin >> x;
+	while(n<10){
+		if(gpa[n]>=x){
+			cout << id[n] << " " << name[n] << " " <<  gpa[n] << "\n";			
+		}
+		n++;
+	}
+
 		
 	return 0;
 }
